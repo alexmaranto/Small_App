c350a044daabcf87a27837b109845dc6cd99242a
alexmaranto <139161834+alexmaranto@users.noreply.github.com>
2023-12-25 17:08:34 -0500

    Add files via upload

---
+++ small-app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from dict import recipes
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/recipes')
+def recipe_list():
+    return render_template('recipe_page.html', recipe_id = recipes)
